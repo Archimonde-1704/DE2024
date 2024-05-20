068f1ae8927ec7c2a8095f38a847bbf76f144338
Archimonde-1704 <k.tas1995@gmail.com>
2024-05-20 23:22:53 +0300

    123

---
+++ DE2/DE2024/Pages/UserInfo.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DE2024.Pages
+{
+    /// <summary>
+    /// Interaction logic for UserInfo.xaml
+    /// </summary>
+    public partial class UserInfo : Page
+    {
+        public UserInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = Window.GetWindow(this) as MainWindow;
+            mainWindow.frame.Navigate(new Login());
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            Singleton.DB.User.ToList();
+            table.ItemsSource = Singleton.DB.User.Local;
+        }
+    }
+}
---
+++ DE2/DE2024/Singleton.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DE2024
+{
+    internal class Singleton
+    {
+        static public DE2024Entities DB = new DE2024Entities();
+    }
+}
